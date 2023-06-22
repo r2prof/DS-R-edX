0d34e7017c2b09777727096b6a095cf5ba013a92
Ali <razaalirafique@gmail.com>
2023-06-22 00:18:29 -0400

    update

--- 01_intro.R
+++ 01_intro.R
@@ -1,18 +1,18 @@
-install.packages("dslabs")
+# CH-01
+#----------------------------
+# install.packages("dslabs")
 library(dslabs)
+
 install.packages("tidyverse")
-# We can install more than one package at once by feeding a character vector
-# to this function:
+
+# We can install more than one package at once by feeding a character vector to this function:
 ## install.packages(c("tidyverse", "dslabs"))
 
-# For example, we just showed how to use the mouse to start a new script, 
-# but you can also use a key binding: Ctrl+Shift+N on Windows and 
-# command+shift+N on the Mac.
+# For example, we just showed how to use the mouse to start a new script, but you can also use a key 
+# binding: Ctrl+Shift+N on Windows and command+shift+N on the Mac.
 
-# Run button on the upper right side of the editing pane. 
-# You can also use the key binding: Ctrl+Shift+Enter
-# To run one line at a time instead of the entire script, 
-# you can use Control-Enter
+# Run button on the upper right side of the editing pane. You can also use the key binding: Ctrl+Shift+Enter
+# To run one line at a time instead of the entire script, you can use Control-Enter
 
 data(murders)
 
